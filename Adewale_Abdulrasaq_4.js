ed628264ddfab3a0c3ba4adcaf899e961502becd
Abdulrasaq07 <adewaleabdulrasak07@gmail.com>
2025-11-28 21:42:49 +0100

    Week 4 Assignment: Created a Server to Host Book API

--- Adewale_Abdulrasaq_4.js
+++ Adewale_Abdulrasaq_4.js
@@ -1,21 +1,6 @@
-const express = require('express');
-const app = express();
+const app = require('./.app')
 const PORT = 3000;
 
-app.get('/users/:id', (req, res) => {
-    const UserId = req.params.id;
-    res.send(`fetching users with id: ${UserId}`)
-})
-
-app.get('/products', (req, res) => {
-    const category = req.query.category;
-    res.send(`The product category is: ${category}`);
-})
-app.listen(3000, () => {
-    console.log(`server is running on http://localhost:${PORT}`);
-})
-app.use(express.json());
-app.post('/login', (req, res) => {
-    const user = req.body;
-    res.send({ message: "Login attempt", user });
+app.listen(PORT, () => {
+    console.log(`Book API running on http://localhost:${PORT}`);
 });
